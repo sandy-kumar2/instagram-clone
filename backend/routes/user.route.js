3ba730aaa01b53cae74e3a50dff12a85ad3a26d4
sandy-kumar2 <imsandeep721@gmail.com>
2024-11-09 16:44:16 +0530

    update

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getProfile, login, logout, register, editProfile, followOrUnfollow, getSuggestedUsers} from "../controllers/user.controller.js";
+import {getProfile, login, logout, register, editProfile, followOrUnfollow,getSuggestedUsers} from "../controllers/user.controller.js";
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 import upload from "../middlewares/multer.js";
 
